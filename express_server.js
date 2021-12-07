cfbe21e000e0b94b66cedba8a536b32ededb64c6
Fatima Siddique <fatimasidq@gmail.com>
2021-12-07 00:12:57 -0500

    removed /hello endpoint

--- express_server.js
+++ express_server.js
@@ -9,10 +9,6 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
