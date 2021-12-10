50685ebef5ea2269fc3e46f510daa6fdf9235fe5
Fatima Siddique <fatimasidq@gmail.com>
2021-12-10 01:49:55 -0500

    created error page

--- express_server.js
+++ express_server.js
@@ -64,7 +64,7 @@ app.post("/urls", (req, res) => {
   const user_id = req.session.user_id;
   if (!user_id) {
     res.statusCode = 405;
-    return res.send("Please log in to shorten a URL.");
+    return res.redirect("/error/Please log in to shorten a URL");
   }
   let shortURL = generateRandomString();
   while (Object.keys(urlDatabase).includes(shortURL)) {
@@ -78,7 +78,7 @@ app.get("/urls/new", (req, res) => {
   const user_id = req.session.user_id;
   if (!user_id) {
     res.statusCode = 405;
-    return res.send("Please log in to shorten a URL.");
+    return res.redirect("/error/Please log in to shorten a URL");
   }
   const templateVars = { email: req.session.email, user_id: user_id }
   res.render("urls_new", templateVars);
@@ -89,11 +89,11 @@ app.post("/register", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  if (!email && password) {
+  if (!email || !password) {
     res.statusCode = 400;
-    return res.send("Fields required!")
+    return res.redirect("/error/Missing a required field!")
   } else if (getUserByEmail(email, users)) {
-    return res.send("Email already registered!")
+    return res.redirect("/error/Email already registered!")
   };
 
   users[random_id] = {
@@ -125,7 +125,7 @@ app.post("/login", (req, res) => {
   const user = getUserByEmail(req.body.email, users);
   if (!user || !bcrypt.compareSync(req.body.password, user.password)) {
     res.statusCode = 403;
-    return res.send("Email or password invalid!");
+    return res.redirect("/error/Email or password invalid!");
   };
   req.session.user_id = user.id;
   req.session.email = req.body.email;
@@ -142,7 +142,7 @@ app.post("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   if (!user_id || user_id !== urlDatabase[shortURL].userID) {
     res.statusCode = 405;
-    return res.send("You do not have permission to complete that action!");
+    return res.redirect("/error/You do not have permission to complete that action!");
   }
   urlDatabase[shortURL].longURL = req.body.newUrl;
   res.redirect(`/urls/${shortURL}`);
@@ -152,7 +152,7 @@ app.get("/urls/:shortURL", (req, res) => {
   const url = urlDatabase[req.params.shortURL];
   if (!url) {
     res.statusCode = 404;
-    return res.send("Shortened URL not found!")
+    return res.redirect("/error/Shortened URL not found!");
   }
   const templateVars = { user_id: req.session.user_id, email: req.session.email, shortURL: req.params.shortURL, url: url };
   res.render("urls_show", templateVars);
@@ -162,7 +162,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   const user_id = req.session.user_id;
   if (!user_id || user_id !== urlDatabase[req.params.shortURL].userID) {
     res.statusCode = 405;
-    return res.send("You do not have permission to complete that action!");
+    return res.redirect("/error/You do not have permission to complete that action!");
   }
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls")
@@ -170,7 +170,17 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 
 app.get("/u/:shortURL", (req, res) => {
   const url = urlDatabase[req.params.shortURL];
-  url ? res.redirect(url.longURL) : res.send("Invalid URL!");
+  url ? res.redirect(url.longURL) : res.redirect("/error/Invalid URL!");
+})
+
+app.get("/error/:errorMessage", (req, res) => {
+  const templateVars = { user_id: req.session.user_id, email: req.session.email, error: req.params.errorMessage};
+  res.render("error", templateVars)
+})
+
+app.get("/:_", (req, res) => {
+  res.statusCode = 404;
+  res.redirect("/error/404 Not Found")
 })
 
 app.listen(PORT, () => {
