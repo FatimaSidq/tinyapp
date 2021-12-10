3d293eb8776e928af55c97d30a50d05542bca728
Fatima Siddique <fatimasidq@gmail.com>
2021-12-10 01:56:31 -0500

    added screenshots

---
+++ helpers.js
@@ -0,0 +1,10 @@
+const getUserByEmail = function(email, users) {
+  if (users == null) {return null}
+  for (let user of Object.values(users)) {
+    if (user.email === email) {
+      return user;
+    }
+  }
+};
+
+module.exports = {getUserByEmail};
